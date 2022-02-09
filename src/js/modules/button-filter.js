b8243a587179bfdd7acd71af53c0b0d6aec8a196
AlexandrShnipov <sshnipov@gmail.com>
2022-02-10 00:15:20 +0300

    added banner about

--- src/js/modules/button-filter.js
+++ src/js/modules/button-filter.js
@@ -1,18 +1,18 @@
 export default function toggleModal() {
-  const filterButton = document.querySelector(".button-modal--oupen");
+  const filterButton = document.querySelector(".button-modal--open");
   const modal = document.querySelector(".modal");
   const close = document.querySelector(".modal__button--closed");
   const filter = document.querySelector(".filter__wrap")
 
-  const closeModal = () => modal.classList.remove("is-oupen");
+  const closeModal = () => modal.classList.remove("is-open");
 
   filterButton.addEventListener("click", () => {
-    modal.classList.add("is-oupen");
+    modal.classList.add("is-open");
   });
   close.addEventListener("click", closeModal);
 
   modal.addEventListener("click", () => {
-    if (document.querySelector(".is-oupen")) {
+    if (document.querySelector(".is-open")) {
       closeModal();
     }
   })
