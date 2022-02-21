d6f56819d955442cc5bb3e0a1d93ef7726f990ca
AlexandrShnipov <sshnipov@gmail.com>
2022-02-21 21:29:19 +0300

    added js for menu-mobile

--- src/js/modules/dist/menu-mobile.dev.js
+++ src/js/modules/dist/menu-mobile.dev.js
@@ -8,20 +8,22 @@ exports["default"] = mobileMenu;
 function mobileMenu() {
   var hamburger = document.getElementById('menu__button');
   var menu = document.querySelector('.menu-mobile');
-  var body = document.querySelector('body');
-  hamburger.addEventListener('click', function () {
-    // hamburger.classList.toggle('active');
-    // console.log('click');
-    menu.classList.toggle('menu-mobile--open');
-    body.classList.toggle('no-scroll');
-  });
-  window.addEventListener('click', function (e) {
-    console.log(e);
+  var modal = document.querySelector('.modal-burger');
+  var body = document.getElementsByTagName('body')[0];
 
-    if (!e.target.classList.contains('menu-mobile--open') && !e.target.matches('.toggle.menu') && !e.currentTarget.matches(hamburger)) {
-      //	hamburger.classList.remove('active');
-      menu.classList.remove('menu-mobile--open');
-      body.classList.remove('no-scroll');
-    }
+  function noScroll() {
+    window.scrollTo(0, 0);
+  }
+
+  hamburger.addEventListener('click', function () {
+    modal.classList.toggle('open');
+    menu.classList.toggle('open');
+    body.classList.toggle('hidden');
+    window.addEventListener('scroll', noScroll);
+  });
+  modal.addEventListener('click', function () {
+    modal.classList.remove('open');
+    menu.classList.remove('open');
+    body.classList.remove('hidden');
   });
 }
--- src/js/modules/menu-mobile.js
+++ src/js/modules/menu-mobile.js
@@ -1,22 +1,26 @@
 export default function mobileMenu() {
-		 const hamburger = document.getElementById('menu__button');
-		 const menu = document.querySelector('.menu-mobile');
-		 const body = document.querySelector('body');
+	const hamburger = document.getElementById('menu__button');
+	const menu = document.querySelector('.menu-mobile');
+	const modal = document.querySelector('.modal-burger');
+	const body = document.getElementsByTagName('body')[0];
 
-		 hamburger.addEventListener('click', () => {
-			 // hamburger.classList.toggle('active');
-				// console.log('click');
-			  menu.classList.toggle('menu-mobile--open');
-			  body.classList.toggle('no-scroll');
-		 });
 
-		 window.addEventListener('click', (e) => {
-			 console.log(e)
-			  if (!e.target.classList.contains('menu-mobile--open') && !e.target.matches('.toggle.menu')  && !e.currentTarget.matches(hamburger)) {
-				//	hamburger.classList.remove('active');
-					menu.classList.remove('menu-mobile--open');
-					body.classList.remove('no-scroll');
-			  }
+	function noScroll() {
+		window.scrollTo(0, 0)
+	}
 
-		 });
+
+	hamburger.addEventListener('click', () => {			
+		modal.classList.toggle('open');
+		menu.classList.toggle('open');
+		body.classList.toggle('hidden');
+		window.addEventListener('scroll', noScroll)
+	});
+
+
+	modal.addEventListener('click', () =>{
+		modal.classList.remove('open');
+		menu.classList.remove('open');
+		body.classList.remove('hidden');
+	})	
 }
